742dc2a5dba328f75e622dea93dcccd6e15588dd
Brian Long <brilong87@gmail.com>
2011-11-16 11:05:52 -0500

    Small update

--- WindowsStuff.sh
+++ WindowsStuff.sh
@@ -1,5 +1,5 @@
-### In Subversion config, i.e. %APPDATA%\Subversion\config
-ssh = C:/Program Files/putty/plink.exe -v -i C:/cygwin/home/BLong/.ssh/putty.ppk -l blong
+### In Subversion config, i.e. %APPDATA%\Subversion\config, -v is for verbose
+ssh = C:/Program Files/putty/plink.exe -v -i C:/cygwin/home/BLong/.ssh/putty.ppk -l <username>
 
 
 
