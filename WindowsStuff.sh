490e65cdd78cccf6fcd902854d1d5882769889e5
Brian Long <brilong87@gmail.com>
2011-11-16 11:24:37 -0500

    Minor change, removed personal item

--- WindowsStuff.sh
+++ WindowsStuff.sh
@@ -1,5 +1,5 @@
 ### In Subversion config, i.e. %APPDATA%\Subversion\config, -v is for verbose
-ssh = C:/Program Files/putty/plink.exe -v -i C:/cygwin/home/BLong/.ssh/putty.ppk -l <username>
+ssh = C:/Program Files/putty/plink.exe -v -i C:/cygwin/home/<username>/.ssh/putty.ppk -l <username>
 
 
 
