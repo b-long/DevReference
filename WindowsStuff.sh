2927eaa7dd20d1d21e9f97cf80e72f7ac4888dee
b-long <b-long@users.noreply.github.com>
2011-12-01 13:03:11 -0500

    Added windows hosts file editing tip.

--- WindowsStuff.sh
+++ WindowsStuff.sh
@@ -1,5 +1,11 @@
 ### In Subversion config, i.e. %APPDATA%\Subversion\config, -v is for verbose
 ssh = C:/Program Files/putty/plink.exe -v -i C:/cygwin/home/<username>/.ssh/putty.ppk -l <username>
 
+### For development purposes, you may need to edit your 'hosts' file.
+### On Windows, it's location is C:\Windows\System32\drivers\etc\hosts
+### The file can only be modified by administrator, so Shift+Right-click 
+### and open an editor (Notepad++) as Administrator.  Then you can add
+### a named host, i.e. if "myhost" is at 192.168.56.7 
+192.168.56.7 myhost
 
 
