e68711a9c7ecc976b4194c01a03e8f3a701e1c18
b-long <b-long@users.noreply.github.com>
2012-01-25 13:14:56 -0500

    Added 'svn status -u'

--- WindowsStuff.bat
+++ WindowsStuff.bat
@@ -5,6 +5,22 @@ REM With an environment variable (used by some svn clients) the same can be acco
 key:   SVN_SSH
 value: "C:/Program Files/putty/plink.exe"  -i C:/cygwin/home/<username>/.ssh/putty.ppk -l <username>
 
+REM       Subversion on the command line:
+: The simplest update 'svn up' or 'svn update'
+svn up
+
+: Print the status of working copy files and directories.
+svn status
+
+: Print the status of working copy files and directories.
+:   --show-updates (or -u) Causes the client to display information about which files in your working copy 
+: are out of date.  This doesn't actually update any of your files—it just shows you which files will be 
+: updated if you then use svn update.
+:
+svn status -u
+
+
+
 : For development purposes, you may need to edit your 'hosts' file.
 REM On Windows, it's location is C:\Windows\System32\drivers\etc\hosts
 REM The file can only be modified by administrator, so Shift+Right-click 
