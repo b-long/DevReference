b5f7ea4665fddc0aecd8da97768693c53a5970e9
b-long <b-long@users.noreply.github.com>
2013-08-27 00:18:53 -0400

    Initial commit gh-pages for DevReference

--- app/main.js
+++ app/main.js
@@ -15,21 +15,30 @@ define(["jquery"], function($) {
 			backgroundColor: 'green'
 		});
 		*/
-		var psuedoIndex = '' +
+		var $mainDiv = $("#devreference-main"),
+			currentYear = new Date().getFullYear(),
+			linkToMyGithub = "http://github.com/b-long/",
+			copywrightAndHeading = ''+
+				'<h3>DevReference - Copywright '+
+				'<a href="' + linkToMyGithub + '">b-long</a> ' + 
+				currentYear + '</h3>';  // Copywright last updated 2013-08-27
+		$mainDiv.append(copywrightAndHeading);
+
+		var libraryContents = '' +
 // TODO: Create this programmatically
 // Start the list
 '<ul id="files">' +
-'<li><a href="/developer-reference/docs/MyShellStuff.sh" class="" title="MyShellStuff.sh">MyShellStuff.sh</a></li>' +
-'<li><a href="/developer-reference/docs/emacs_REFERENCE.md" class="" title="emacs_REFERENCE.md">emacs_REFERENCE.md</a></li>' +
-'<li><a href="/developer-reference/docs/git_REFERENCE.md" class="" title="git_REFERENCE.md">git_REFERENCE.md</a></li>' +
-'<li><a href="/developer-reference/docs/oracle_REFERENCE.md" class="" title="oracle_REFERENCE.md">oracle_REFERENCE.md</a></li>' +
-'<li><a href="/developer-reference/docs/rpm_REFERENCE.md" class="" title="rpm_REFERENCE.md">rpm_REFERENCE.md</a></li>' +
-'<li><a href="/developer-reference/docs/rsync_REFERENCE.md" class="" title="rsync_REFERENCE.md">rsync_REFERENCE.md</a></li>' +
-'<li><a href="/developer-reference/docs/svn_REFERENCE.md" class="" title="svn_REFERENCE.md">svn_REFERENCE.md</a></li>' +
-'<li><a href="/developer-reference/docs/vim_REFERENCE.md" class="" title="vim_REFERENCE.md">vim_REFERENCE.md</a></li>' +
+'<li><a href="/library/MyShellStuff.sh" class="" title="MyShellStuff.sh">MyShellStuff.sh</a></li>' +
+'<li><a href="/library/emacs_REFERENCE.md" class="" title="emacs_REFERENCE.md">emacs_REFERENCE.md</a></li>' +
+'<li><a href="/library/git_REFERENCE.md" class="" title="git_REFERENCE.md">git_REFERENCE.md</a></li>' +
+'<li><a href="/library/oracle_REFERENCE.md" class="" title="oracle_REFERENCE.md">oracle_REFERENCE.md</a></li>' +
+'<li><a href="/library/rpm_REFERENCE.md" class="" title="rpm_REFERENCE.md">rpm_REFERENCE.md</a></li>' +
+'<li><a href="/library/rsync_REFERENCE.md" class="" title="rsync_REFERENCE.md">rsync_REFERENCE.md</a></li>' +
+'<li><a href="/library/svn_REFERENCE.md" class="" title="svn_REFERENCE.md">svn_REFERENCE.md</a></li>' +
+'<li><a href="/library/vim_REFERENCE.md" class="" title="vim_REFERENCE.md">vim_REFERENCE.md</a></li>' +
 // End the list
 '</ul>';
-		$("#devreference-main").append(psuedoIndex);
+		$mainDiv.append(libraryContents);
 
 		// For dev purposes, output the current jQuery versioni
 		console.log('jQuery ' + $.fn.jquery + ' loaded!');
--- dist/require.js
+++ dist/require.js
@@ -1,5 +1,5 @@
-/*! devreference - v0.0.0 - 2013-08-11
-* Copyright (c) 2013 Author Name; Licensed MIT */
+/*! DevReference - v0.0.0 - 2013-08-26
+* Copyright (c) 2013 b-long; Licensed MIT */
 //Not using strict: uneven strict support in browsers, #392, and causes
 //problems with requirejs.exec()/transpiler plugins that may not be strict.
 /*jslint regexp: true, nomen: true, sloppy: true */
@@ -10880,20 +10880,41 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
 
 })( window );
 
+/*global console*/
+
 // to depend on a bower installed component:
 // define(['component/componentName/file'])
 
 define('main',["jquery"], function($) {
-  $('body').append('jQuery ' + $.fn.jquery + ' loaded!');
   
 	$(document).ready(function(){
-		
+		/*
+		TODO: Move this to a unit test.  
+		// Confirms that jQuery can manipulate the DOM at this point
 		$('#devreference-main').css({
 			height: '200px',
 			width: '200px',
 			backgroundColor: 'green'
 		});
+		*/
+		var psuedoIndex = '' +
+// TODO: Create this programmatically
+// Start the list
+'<ul id="files">' +
+'<li><a href="/library/MyShellStuff.sh" class="" title="MyShellStuff.sh">MyShellStuff.sh</a></li>' +
+'<li><a href="/library/emacs_REFERENCE.md" class="" title="emacs_REFERENCE.md">emacs_REFERENCE.md</a></li>' +
+'<li><a href="/library/git_REFERENCE.md" class="" title="git_REFERENCE.md">git_REFERENCE.md</a></li>' +
+'<li><a href="/library/oracle_REFERENCE.md" class="" title="oracle_REFERENCE.md">oracle_REFERENCE.md</a></li>' +
+'<li><a href="/library/rpm_REFERENCE.md" class="" title="rpm_REFERENCE.md">rpm_REFERENCE.md</a></li>' +
+'<li><a href="/library/rsync_REFERENCE.md" class="" title="rsync_REFERENCE.md">rsync_REFERENCE.md</a></li>' +
+'<li><a href="/library/svn_REFERENCE.md" class="" title="svn_REFERENCE.md">svn_REFERENCE.md</a></li>' +
+'<li><a href="/library/vim_REFERENCE.md" class="" title="vim_REFERENCE.md">vim_REFERENCE.md</a></li>' +
+// End the list
+'</ul>';
+		$("#devreference-main").append(psuedoIndex);
 
+		// For dev purposes, output the current jQuery versioni
+		console.log('jQuery ' + $.fn.jquery + ' loaded!');
 	});
 
 });
