58b6cf7bd4525b96c35899e10578f7e5415e8cb1
b-long <b-long@users.noreply.github.com>
2014-02-19 16:38:42 -0500

    Added more Maven commands
    Added Maven commands for offline mode & downloading dependency javadocs.

--- MyShellStuff.sh
+++ MyShellStuff.sh
@@ -101,6 +101,8 @@ git log --graph origin/<feature branch name>..HEAD
 ###
 ### Maven
 ###
+# Use Maven in offline mode (-o or --offline)
+mvn -o package
 # Install my service module, building and installing persistence and common code first
 # cd to the top of the multi-module project 
 mvn -pl group:project-commons,group:project-persistence,group:project-services install
@@ -111,7 +113,8 @@ mvn -pl group:project-persistence -amd install
 # A bit more fancy, build (and install) project-services and everything 
 # it depends on, while outputting to standard out and a log file
 mvn -pl group:project-services -am install 2>&1 | tee build.log
-
+# Download all of the javadocs for your dependencies
+mvn dependency:resolve -Dclassifier=javadoc
 # Package this module and then scp the .war's to deploy on a jboss server
 mvn package && scp $(find . -name "*.war") <user>@<host name>:/srv/jboss-6.0.0/server/default/deploy/
 # Clean & rebuild everything, then run 1 integration test class
